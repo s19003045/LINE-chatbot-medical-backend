4495def2be66b9aa9b18a72f564a1091291970ec
Gary Wu <s19003045@gmail.com>
2020-06-21 23:42:42 +0800

    重構 buttons template
    重點：
    1. 修正 postback action 及 datetime picker action 的內容。
    2. postback action 的 text 屬性(官方不建議)改成 displayText 屬性(官方建議)。

--- app.js
+++ app.js
@@ -164,6 +164,84 @@ function handlePostback(event) {
       })
       return replyText(event.replyToken, statistic_ouput);
 
+    case 'flyToMoon':
+      if (_data.flyToMoon === 'true') {
+        // 詢問登陸月球日期
+        return client.replyMessage(
+          event.replyToken,
+          {
+            type: 'template',
+            altText: '登陸月球',
+            template: {
+              type: 'buttons',
+              thumbnailImageUrl: `${imageURL}/buttons/dinosaur_space.png`,
+              title: '坐太空梭登陸月球',
+              text: '選個登陸月球日期',
+              actions: [
+                {
+                  "type": "datetimepicker",
+                  "label": "出發日期",
+                  "data": "action=departure_to_moon&type=DATETIME",
+                  "mode": "datetime",
+                  "initial": "2020-06-01t00:00",
+                  "max": "2020-12-01t23:59",
+                  "min": "2020-06-01t00:00"
+                },
+                {
+                  "type": "datetimepicker",
+                  "label": "回程日期",
+                  "data":
+                    "action=return_from_moon&type=DATETIME",
+                  "mode": "datetime",
+                  "initial": "2020-06-01t00:00",
+                  "max": "2020-12-01t23:59",
+                  "min": "2020-06-01t00:00"
+                }
+              ]
+            }
+          })
+      } else {
+        return client.replyMessage(
+          event.replyToken,
+          {
+            type: 'template',
+            altText: 'VR 探險月球',
+            template: {
+              type: 'buttons',
+              thumbnailImageUrl: `${imageURL}/buttons/dinosaur_music.png`,
+              title: 'VR 探險月球',
+              text: '要來個 VR 探險月球嗎？',
+              actions: [
+                {
+                  type: 'postback',
+                  label: '我要 VR 探險月球',
+                  displayText: '我要 VR 探險月球',
+                  data: 'action=vr_moon&VRmoon=true'
+                },
+                {
+                  type: 'postback',
+                  label: '我不要VR探險',
+                  displayText: '我不要VR探險',
+                  data: 'action=vr_moon&VRmoon=false'
+                }
+              ]
+            }
+          }
+        )
+      }
+
+    case 'departure_to_moon':
+      return replyText(event.replyToken, `出發去月球時間 : ${JSON.stringify(event.postback.params)}`)
+
+    case 'return_from_moon':
+      return replyText(event.replyToken, `從月球回程出發時間 : ${JSON.stringify(event.postback.params)}`)
+
+    case 'vr_moon':
+      if (_data.VRmoon === 'true') {
+        return replyText(event.replyToken, '歡迎你隨時前來本館體驗 VR 探險月球！')
+      } else {
+        return replyText(event.replyToken, '感謝你的回覆！')
+      }
     default:
       return replyText(event.replyToken, `Got postback: ${data}`);
   }
@@ -330,77 +408,12 @@ function handleText(message, replyToken, source) {
             actions: [
               { label: '太陽系介紹', type: 'uri', uri: 'https://youtu.be/libKVRa01L8' },
               { label: '銀河系之美', type: 'uri', uri: 'https://youtu.be/4VX6Nh6YLYk' },
-              { label: '我要登陸月球', type: 'postback', text: '我要登陸月球', data: 'action=fly&gotomoon=true' },
-              { label: '我不要登陸月球', type: 'postback', text: '我不要登陸月球', data: 'action=null&gotomoon=false' }
-
+              { label: '我要登陸月球', type: 'postback', displayText: '我要登陸月球', data: 'action=flyToMoon&flyToMoon=true' },
+              { label: '我不要登陸月球', type: 'postback', displayText: '我不要登陸月球', data: 'action=flyToMoon&flyToMoon=false' }
             ],
           },
         }
       );
-
-    // 詢問登陸月球日期
-    case '我要登陸月球':
-      return client.replyMessage(
-        replyToken,
-        {
-          type: 'template',
-          altText: '登陸月球',
-          template: {
-            type: 'buttons',
-            thumbnailImageUrl: `${imageURL}/buttons/dinosaur_space.png`,
-            title: '坐太空梭登陸月球',
-            text: '選個登陸月球日期',
-            actions: [
-              {
-                "type": "datetimepicker",
-                "label": "出發日期",
-                "data": "DATETIME",
-                "mode": "datetime",
-                "initial": "2020-06-01t00:00",
-                "max": "2020-12-01t23:59",
-                "min": "2020-06-01t00:00"
-              },
-              {
-                "type": "datetimepicker",
-                "label": "回程日期",
-                "data": "DATETIME",
-                "mode": "datetime",
-                "initial": "2020-06-01t00:00",
-                "max": "2020-12-01t23:59",
-                "min": "2020-06-01t00:00"
-              }
-            ]
-          }
-        })
-
-    case '我不要登陸月球':
-      return client.replyMessage(
-        replyToken,
-        {
-          type: 'template',
-          altText: 'VR 探險月球',
-          template: {
-            type: 'buttons',
-            thumbnailImageUrl: `${imageURL}/buttons/dinosaur_music.png`,
-            title: 'VR 探險月球',
-            text: '要來個 VR 探險月球嗎？',
-            actions: [
-              {
-                type: 'postback',
-                label: '我要 VR 探險月球',
-                text: '我要 VR 探險月球',
-                data: 'vr_moon=true'
-              },
-              {
-                type: 'postback',
-                label: '我不要VR探險',
-                text: '我不要VR探險',
-                data: 'vr_moon=false'
-              }
-            ]
-          }
-        }
-      )
 
     // confirm template
     case 'confirm':
