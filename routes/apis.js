507ef6a40cc3bfeb9aa57bb6f7c90402bf3e24f3
Gary Wu <s19003045@gmail.com>
2020-07-06 19:42:01 +0800

    設定關鍵字及回應訊息-主控台 API-修改 路由

--- routes/apis.js
+++ routes/apis.js
@@ -4,13 +4,13 @@ const replyMsgController = require('../controllers/apis/replyMsgController')
 
 
 // 建立 text event 及對應的 reply message 
-router.post('/keyword-reply', replyMsgController.createKeywordReply)
+router.post('/bots/:botId/keyword-reply', replyMsgController.createKeywordReply)
 // 建立 text event 及對應的 reply message 
-router.get('/keyword-reply', replyMsgController.getKeywordReply)
+router.get('/bots/:botId/keyword-reply', replyMsgController.getKeywordReply)
 // 修改 text event 及對應的 reply message 
-router.put('/keyword-reply', replyMsgController.putKeywordReply)
+router.put('/bots/:botId/keyword-reply', replyMsgController.putKeywordReply)
 // 封存針對 text event 的 reply message 
-router.delete('/keyword-reply', replyMsgController.deleteKeywordReply)
+router.delete('/bots/:botId/keyword-reply', replyMsgController.deleteKeywordReply)
 
 
 
