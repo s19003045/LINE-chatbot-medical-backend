235f48b1d950a6fdf1f881dacfe37adfdf9cba71
Gary Wu <s19003045@gmail.com>
2020-07-06 18:10:54 +0800

    修改種子清單：textEvent, replyMessage

--- seeders/20200706034411-textEvent-seed.js
+++ seeders/20200706034411-textEvent-seed.js
@@ -1,10 +1,11 @@
 'use strict';
+const { v4: uuidv4 } = require('uuid');
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('TextEvents', [
       {
-        uuid: 'd59e9540-dabe-471c-9a9c-d4a08f87fe99', //使用於主控台
+        uuid: uuidv4(), //使用於主控台
         text: '冰淇淋', //關鍵字
         ReplyMessageId: 1, //FK
         ChatbotId: 1, //FK
@@ -12,7 +13,7 @@ module.exports = {
         updatedAt: new Date()
       },
       {
-        uuid: 'd59e9540-dabe-471c-9a9c-d4a08f87fe99', //使用於主控台
+        uuid: uuidv4(), //使用於主控台
         text: '香蕉', //關鍵字
         ReplyMessageId: 1, //FK
         ChatbotId: 1, //FK
@@ -20,7 +21,7 @@ module.exports = {
         updatedAt: new Date()
       },
       {
-        uuid: 'd59e9540-dabe-471c-9a9c-d4a08f87fe99', //使用於主控台
+        uuid: uuidv4(), //使用於主控台
         text: '芒果', //關鍵字
         ReplyMessageId: 1, //FK
         ChatbotId: 1, //FK
