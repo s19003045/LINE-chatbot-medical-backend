3836a1e586c01e2c3cc8ae7b28c3b2149ef0e234
Gary Wu <s19003045@gmail.com>
2020-07-06 23:16:31 +0800

    設定關鍵字及回應訊息-主控台 API-新增路由(新增及刪除 module keyword、reply message、text event)
    新增及刪除 module keyword、reply message、text event

--- routes/apis.js
+++ routes/apis.js
@@ -2,14 +2,27 @@ const express = require('express')
 const router = express.Router()
 const replyMsgController = require('../controllers/apis/replyMsgController')
 
+// 新增 module keyword
+router.post('/bots/:botId/module-keyword', replyMsgController.createModuleKeyword)
+// 刪除 module keyword
+router.delete('/bots/:botId/module-keyword', replyMsgController.deleteModuleKeyword)
+// 新增 reply message
+router.post('/bots/:botId/reply-message', replyMsgController.createReplyMessage)
+// 刪除 reply message
+router.post('/bots/:botId/reply-message', replyMsgController.deleteReplyMessage)
+// 新增 text event
+router.post('/bots/:botId/text-event', replyMsgController.createTextEvent)
+// 刪除 text event
+router.delete('/bots/:botId/text-event', replyMsgController.deleteTextEvent)
 
-// 建立 text event 及對應的 reply message 
+
+// 儲存關鍵字回應模組
 router.post('/bots/:botId/keyword-reply', replyMsgController.createKeywordReply)
-// 建立 text event 及對應的 reply message 
+// 取得關鍵字回應模組
 router.get('/bots/:botId/keyword-reply', replyMsgController.getKeywordReply)
-// 修改 text event 及對應的 reply message 
+// 修改關鍵字回應模組
 router.put('/bots/:botId/keyword-reply', replyMsgController.putKeywordReply)
-// 封存針對 text event 的 reply message 
+// 刪除關鍵字回應模組
 router.delete('/bots/:botId/keyword-reply', replyMsgController.deleteKeywordReply)
 
 
