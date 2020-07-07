ff8f982398ed2873a1b62045255fa2b1d0a86fc9
Gary Wu <s19003045@gmail.com>
2020-07-07 11:08:14 +0800

    資料表異動：ModuleKeyword 新增 moduleUsedCount 、moduleReadCount 欄位

---
+++ migrations/20200707024135-add-column-to-ModuleKeyword.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    queryInterface.addColumn('ModuleKeywords', 'moduleUsedCount', Sequelize.INTEGER, {
+      after: 'status'
+    });
+    return queryInterface.addColumn('ModuleKeywords', 'moduleReadCount', Sequelize.INTEGER, {
+      after: 'status'
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.removeColumn('ModuleKeywords', 'moduleUsedCount');
+  }
+};
--- models/modulekeyword.js
+++ models/modulekeyword.js
@@ -4,6 +4,14 @@ module.exports = (sequelize, DataTypes) => {
     name: DataTypes.STRING, //模組名稱
     uuid: DataTypes.STRING,
     status: DataTypes.ENUM('edited', 'in-use', 'archived'),  //模組狀態
+    moduleUsedCount: {
+      type: DataTypes.INTEGER, // 使用次數
+      defaultValue: 0
+    },
+    moduleReadCount: {
+      type: DataTypes.INTEGER, // 已次次數
+      defaultValue: 0
+    },
     ChatbotId: DataTypes.INTEGER,
   }, {
     paranoid: true,
