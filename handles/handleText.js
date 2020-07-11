cccee18a62ef320ab366b3a3941a8276be606911
Gary Wu <s19003045@gmail.com>
2020-07-11 23:58:15 +0800

    修正 handle textEent 邏輯

--- handles/handleText.js
+++ handles/handleText.js
@@ -6,11 +6,47 @@ let baseURL = process.env.BASE_URL;
 const flexBubble = require('../templates/flexMessages/flex_bubble')
 const flexCarousel = require('../templates/flexMessages/flex_carousel')
 
+const db = require('../models')
+const TextEvent = db.TextEvent
+const ReplyMessage = db.ReplyMessage
+const { Op } = require("sequelize")
+
 function handleText(message, replyToken, source, client, replyText) {
   let replyMsg
   const imageURL = `${baseURL}/public/images`
   const videoURL = `${baseURL}/public/videos`
   const audioURL = `${baseURL}/public/audios`
+
+  //關鍵字相符情形：等於、包含、不包含
+  //文字開頭是、文字結尾是
+  //數字相符情形：>=、>、=、<=、<
+  console.log(message.text)
+  //字串完全相符
+  return TextEvent.findOne({
+    where: {
+      text: message.text
+    },
+    include: [
+      {
+        model: ReplyMessage
+      }
+    ]
+  })
+    .then((textEvent) => {
+      console.log('textEvent:', textEvent)
+      if (textEvent) {
+        return client.replyMessage(replyToken, textEvent.ReplyMessage.messageTemplate)
+      } else {
+        return client.replyMessage(replyToken, {
+          type: 'text',
+          text: 'No keyword like'
+        })
+      }
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+
   switch (message.text) {
     // send text
     case 'hello':
