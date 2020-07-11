5b79e892d1577d03c9449e2be4be856563a164a8
Gary Wu <s19003045@gmail.com>
2020-07-11 20:40:00 +0800

    Postback 回應模組-後端-Model-ModulePostback

---
+++ migrations/20200711085439-create-module-postback.js
@@ -0,0 +1,47 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('ModulePostBacks', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      uuid: {
+        type: Sequelize.STRING
+      },
+      moduleUsedCount: {
+        type: Sequelize.INTEGER,
+        defaultValue: 0
+      },
+      moduleReadCount: {
+        type: Sequelize.INTEGER,
+        defaultValue: 0
+      },
+      status: {
+        type: Sequelize.ENUM('edited', 'in-use', 'archived')
+      },
+      ChatbotId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      deletedAt: {
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('ModulePostBacks');
+  }
+};
---
+++ models/modulepostback.js
@@ -0,0 +1,19 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const ModulePostBack = sequelize.define('ModulePostBack', {
+    name: DataTypes.STRING, // postback 名稱
+    uuid: DataTypes.STRING,
+    moduleUsedCount: DataTypes.INTEGER, // 使用次數
+    moduleReadCount: DataTypes.INTEGER, // 已讀次數
+    status: DataTypes.ENUM('edited', 'in-use', 'archived'),  //模組狀態
+    ChatbotId: DataTypes.INTEGER  // FK
+  }, {
+    paranoid: true,
+  });
+  ModulePostBack.associate = function (models) {
+    ModulePostBack.hasMany(models.PostBackEvent)
+    ModulePostBack.hasOne(models.ReplyMessage)
+    ModulePostBack.belongsTo(models.Chatbot)
+  };
+  return ModulePostBack;
+};
