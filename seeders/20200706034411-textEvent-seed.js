aa1372227a06c517fc8aa11871981d68dd2c3411
Gary Wu <s19003045@gmail.com>
2020-07-13 17:13:20 +0800

    Postback 回應模組-後端-修正 handlePostback 與 handleText 衝突而造成 quick reply 無法正常顯示
    解決方法：
    若有設定 postbackEvent 時，需同時在 textEvent 設定，共同指向相同的 replyMessage

--- seeders/20200706034411-textEvent-seed.js
+++ seeders/20200706034411-textEvent-seed.js
@@ -2,8 +2,8 @@
 const { v4: uuidv4 } = require('uuid');
 const ChatbotId = 1
 
-const ReplyMessageIds = [1, 5, 6, 7, 8, 9]
-const ModuleKeywordIds = [1, 2, 3, 4, 5, 6]
+const ReplyMessageIds = [1, 5, 6, 7, 8, 9, 2, 3, 4]
+const ModuleKeywordIds = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 const autoIncrementNum = 10; //local DB 設定1，若為 heroku mySQL DB 則須改為10
 
 module.exports = {
@@ -66,6 +66,36 @@ module.exports = {
         ReplyMessageId: 1 + autoIncrementNum * (ReplyMessageIds[5] - 1), //FK
         ChatbotId: ChatbotId, //FK
         ModuleKeywordId: 1 + autoIncrementNum * (ModuleKeywordIds[5] - 1),  //FK
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        uuid: uuidv4(), //使用於主控台
+        text: '查詢健保給付項目', //關鍵字
+        textEventCount: 0, //使用次數
+        ReplyMessageId: 1 + autoIncrementNum * (ReplyMessageIds[6] - 1), //FK
+        ChatbotId: ChatbotId, //FK
+        ModuleKeywordId: 1 + autoIncrementNum * (ModuleKeywordIds[6] - 1),  //FK
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        uuid: uuidv4(), //使用於主控台
+        text: '查詢自費項目', //關鍵字
+        textEventCount: 0, //使用次數
+        ReplyMessageId: 1 + autoIncrementNum * (ReplyMessageIds[7] - 1), //FK
+        ChatbotId: ChatbotId, //FK
+        ModuleKeywordId: 1 + autoIncrementNum * (ModuleKeywordIds[7] - 1),  //FK
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        uuid: uuidv4(), //使用於主控台
+        text: '依週期數查詢產檢項目', //關鍵字
+        textEventCount: 0, //使用次數
+        ReplyMessageId: 1 + autoIncrementNum * (ReplyMessageIds[8] - 1), //FK
+        ChatbotId: ChatbotId, //FK
+        ModuleKeywordId: 1 + autoIncrementNum * (ModuleKeywordIds[8] - 1),  //FK
         createdAt: new Date(),
         updatedAt: new Date()
       }
--- seeders/20200706054600-moduleKeyword-seed.js
+++ seeders/20200706054600-moduleKeyword-seed.js
@@ -2,8 +2,6 @@
 const { v4: uuidv4 } = require('uuid');
 const ChatbotId = 1
 
-const ReplyMessageIds = [1, 5, 6, 7, 8, 9]
-const ModuleKeywordIds = [1, 2, 3, 4, 5, 6]
 const autoIncrementNum = 10; //local DB 設定1，若為 heroku mySQL DB 則須改為10
 
 module.exports = {
@@ -51,6 +49,30 @@ module.exports = {
       },
       {
         name: '更多資訊', //模組名稱
+        uuid: uuidv4(),
+        status: 'in-use',
+        ChatbotId: ChatbotId,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: '查詢健保給付項目', //模組名稱
+        uuid: uuidv4(),
+        status: 'in-use',
+        ChatbotId: ChatbotId,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: '查詢自費項目', //模組名稱
+        uuid: uuidv4(),
+        status: 'in-use',
+        ChatbotId: ChatbotId,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: '依週期數查詢產檢項目', //模組名稱
         uuid: uuidv4(),
         status: 'in-use',
         ChatbotId: ChatbotId,
