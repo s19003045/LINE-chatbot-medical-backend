a83349c3f6d37531b066050e431b32905affd739
Gary Wu <s19003045@gmail.com>
2020-07-13 22:48:10 +0800

    Postback 回應模組分析-後端-使用次數increment

--- handles/handlePostback.js
+++ handles/handlePostback.js
@@ -4,7 +4,9 @@
 const db = require('../models')
 const PostBackEvent = db.PostBackEvent
 const ReplyMessage = db.ReplyMessage
+const ModulePostBack = db.ModulePostBack
 
+// import helpers
 const querystring = require('querystring');
 // base URL for webhook server
 let baseURL = process.env.BASE_URL;
@@ -29,16 +31,36 @@ function handlePostback(event, client, replyText) {
       data: _data.data
     },
     include: [
-      { model: ReplyMessage }
+      { model: ReplyMessage },
+      { model: ModulePostBack }
     ]
   })
-    .then((postBackEvent) => {
-      if (postBackEvent) {
-        return client.replyMessage(event.replyToken, postBackEvent.ReplyMessage.messageTemplate)
-      } else {
-        return client.replyMessage(event.replyToken, {
+    .then(async (postBackEvent) => {
+      try {
+        if (postBackEvent) {
+          //replyMessage 使用次數+1
+          const replyMessageIncrement = await postBackEvent.ReplyMessage.increment(['replyMsgCount'], {
+            by: 1
+          })
+          //module 使用次數+1
+          const modulePostBackIncrement = await postBackEvent.ModulePostBack.increment(['moduleUsedCount'], {
+            by: 1
+          })
+
+          if (replyMessageIncrement && modulePostBackIncrement) {
+            return client.replyMessage(event.replyToken, postBackEvent.ReplyMessage.messageTemplate)
+          }
+        } else {
+          return client.replyMessage(event.replyToken, {
+            type: 'text',
+            text: '針對這個問題，我還沒辦法回答，你要不要先問我別的問題？'
+          })
+        }
+      } catch (err) {
+        console.log(err)
+        return client.replyMessage(replyToken, {
           type: 'text',
-          text: '針對這個問題，我還沒辦法回答，你要不要先問我別的問題？'
+          text: '這個問題我還沒辦法回答，你要不要先問我別的問題？'
         })
       }
     })
