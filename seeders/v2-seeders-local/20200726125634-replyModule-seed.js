098a89d8fdebd846f24b187f1bdb90d1540934ec
Gary Wu <s19003045@gmail.com>
2020-07-26 22:37:33 +0800

    新增 local 及 remote 種子資料-replyModule 及 keyword

---
+++ seeders/v2-seeders-local/20200726125634-replyModule-seed.js
@@ -0,0 +1,141 @@
+'use strict';
+const { v4: uuidv4 } = require('uuid');
+const ChatbotId = 1
+
+
+//1
+const prenatalVisit = require('../../templates/replyModule_ReplyMessage/產前檢查.json')
+
+//2
+const healthInsuranceExam = require('../../templates/replyModule_ReplyMessage/查詢健保給付項目.json')
+
+//3
+const atOnesOwnExpenseExam = require('../../templates/replyModule_ReplyMessage/查詢自費項目.json')
+
+//4
+const queryExamByWeeks = require('../../templates/replyModule_ReplyMessage/依週期數查詢產檢項目.json')
+
+//5  孕期衛教.json
+const pregnancyEducation = require('../../templates/replyModule_ReplyMessage/孕期衛教.json')
+
+//6 胎兒成長.json
+const fetusGrowth = require('../../templates/replyModule_ReplyMessage/胎兒成長.json')
+
+//7 生理記錄.json
+const healthRecord = require('../../templates/replyModule_ReplyMessage/生理記錄.json')
+
+//8 問與答.json
+const questionAndAnswer = require('../../templates/replyModule_ReplyMessage/問與答.json')
+
+//9 更多資訊.json
+const moreInformation = require('../../templates/replyModule_ReplyMessage/更多資訊.json')
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('ReplyModules', [
+      {
+        name: '產前檢查', // 模組名稱
+        uuid: uuidv4(), // 使用於主控台
+        // moduleUsedCount: 0, // (略，已帶 default value)
+        replyMessage:
+          JSON.stringify(prenatalVisit), //id:1
+        status: 'in-use', //此模組的狀態
+        ChatbotId: ChatbotId, //FK
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: '查詢健保給付項目', // 模組名稱
+        uuid: uuidv4(), // 使用於主控台
+        // moduleUsedCount: 0, // (略，已帶 default value)
+        replyMessage:
+          JSON.stringify(healthInsuranceExam), //id:2
+        status: 'in-use', //此模組的狀態
+        ChatbotId: ChatbotId, //FK
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: '查詢自費項目', // 模組名稱
+        uuid: uuidv4(), // 使用於主控台
+        // moduleUsedCount: 0, // (略，已帶 default value)
+        replyMessage:
+          JSON.stringify(atOnesOwnExpenseExam), //id:3
+        status: 'in-use', //此模組的狀態
+        ChatbotId: ChatbotId, //FK
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: '依週期數查詢產檢項目', // 模組名稱
+        uuid: uuidv4(), // 使用於主控台
+        // moduleUsedCount: 0, // (略，已帶 default value)
+        replyMessage:
+          JSON.stringify(queryExamByWeeks), //id:4
+        status: 'in-use', //此模組的狀態
+        ChatbotId: ChatbotId, //FK
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: '孕期衛教', // 模組名稱
+        uuid: uuidv4(), // 使用於主控台
+        // moduleUsedCount: 0, // (略，已帶 default value)
+        replyMessage:
+          JSON.stringify(pregnancyEducation), //id:5
+        status: 'in-use', //此模組的狀態
+        ChatbotId: ChatbotId, //FK
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: '胎兒成長', // 模組名稱
+        uuid: uuidv4(), // 使用於主控台
+        // moduleUsedCount: 0, // (略，已帶 default value)
+        replyMessage:
+          JSON.stringify(fetusGrowth), //id:6
+        status: 'in-use', //此模組的狀態
+        ChatbotId: ChatbotId, //FK
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: '生理記錄', // 模組名稱
+        uuid: uuidv4(), // 使用於主控台
+        // moduleUsedCount: 0, // (略，已帶 default value)
+        replyMessage:
+          JSON.stringify(healthRecord), //id:7
+        status: 'in-use', //此模組的狀態
+        ChatbotId: ChatbotId, //FK
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: '問與答', // 模組名稱
+        uuid: uuidv4(), // 使用於主控台
+        // moduleUsedCount: 0, // (略，已帶 default value)
+        replyMessage:
+          JSON.stringify(questionAndAnswer), //id:8
+        status: 'in-use', //此模組的狀態
+        ChatbotId: ChatbotId, //FK
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: '更多資訊', // 模組名稱
+        uuid: uuidv4(), // 使用於主控台
+        // moduleUsedCount: 0, // (略，已帶 default value)
+        replyMessage:
+          JSON.stringify(moreInformation), //id:9
+        status: 'in-use', //此模組的狀態
+        ChatbotId: ChatbotId, //FK
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+    ], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('ReplyModules', null, {});
+  }
+};
---
+++ seeders/v2-seeders-local/20200726125742-keyword-seed.js
@@ -0,0 +1,91 @@
+'use strict';
+const { v4: uuidv4 } = require('uuid');
+const ChatbotId = 1
+
+const ReplyModuleIds = [1, 5, 6, 7, 8, 9, 2, 3, 4]
+
+const autoIncrementNum = 1; //local DB 設定1，若為 heroku mySQL DB 則須改為10
+
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Keywords', [
+      {
+        name: '產前檢查', //關鍵字
+        ChatbotId: ChatbotId, //FK
+        // UsedCount: 0, // (略，已帶 default value)
+        triggerModuleId: 1 + autoIncrementNum * (ReplyModuleIds[0] - 1),  //FK
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: '孕期衛教', //關鍵字
+        ChatbotId: ChatbotId, //FK
+        // UsedCount: 0, // (略，已帶 default value)
+        triggerModuleId: 1 + autoIncrementNum * (ReplyModuleIds[1] - 1),  //FK
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: '胎兒成長', //關鍵字
+        ChatbotId: ChatbotId, //FK
+        // UsedCount: 0, // (略，已帶 default value)
+        triggerModuleId: 1 + autoIncrementNum * (ReplyModuleIds[2] - 1),  //FK
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: '生理記錄', //關鍵字
+        ChatbotId: ChatbotId, //FK
+        // UsedCount: 0, // (略，已帶 default value)
+        triggerModuleId: 1 + autoIncrementNum * (ReplyModuleIds[3] - 1),  //FK
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: '問與答', //關鍵字
+        ChatbotId: ChatbotId, //FK
+        // UsedCount: 0, // (略，已帶 default value)
+        triggerModuleId: 1 + autoIncrementNum * (ReplyModuleIds[4] - 1),  //FK
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: '更多資訊', //關鍵字
+        ChatbotId: ChatbotId, //FK
+        // UsedCount: 0, // (略，已帶 default value)
+        triggerModuleId: 1 + autoIncrementNum * (ReplyModuleIds[5] - 1),  //FK
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: '查詢健保給付項目', //關鍵字
+        ChatbotId: ChatbotId, //FK
+        // UsedCount: 0, // (略，已帶 default value)
+        triggerModuleId: 1 + autoIncrementNum * (ReplyModuleIds[6] - 1),  //FK
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: '查詢自費項目', //關鍵字
+        ChatbotId: ChatbotId, //FK
+        // UsedCount: 0, // (略，已帶 default value)
+        triggerModuleId: 1 + autoIncrementNum * (ReplyModuleIds[7] - 1),  //FK
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: '依週期數查詢產檢項目', //關鍵字
+        ChatbotId: ChatbotId, //FK
+        // UsedCount: 0, // (略，已帶 default value)
+        triggerModuleId: 1 + autoIncrementNum * (ReplyModuleIds[8] - 1),  //FK
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('Keywords', null, {});
+  }
+};
