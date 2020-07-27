058e0d9cba850dea652d0d1390ba13e7a6a42700
Gary Wu <s19003045@gmail.com>
2020-07-27 18:27:39 +0800

    機器人腳本-修正 handlePostBack 邏輯

--- handles/handlePostback.js
+++ handles/handlePostback.js
@@ -2,67 +2,29 @@
 
 // import model
 const db = require('../models')
-const PostBackEvent = db.PostBackEvent
-const ReplyMessage = db.ReplyMessage
-const ModulePostBack = db.ModulePostBack
+const ReplyModule = db.ReplyModule
 
 // import helpers
 const querystring = require('querystring');
+const triggerReply = require('./postbackHelper/triggerReply.js')
+
 // base URL for webhook server
 let baseURL = process.env.BASE_URL;
 
 // 處理 postback 訊息
 function handlePostback(event, client, replyText) {
-  const imageURL = `${baseURL}/public/images`
   let data = event.postback.data
   let _data = querystring.parse(data)
 
-  return PostBackEvent.findOne({
-    where: {
-      subject: _data.subject,
-      data: _data.data
-    },
-    include: [
-      { model: ReplyMessage },
-      { model: ModulePostBack }
-    ]
-  })
-    .then(async (postBackEvent) => {
-      try {
-        if (postBackEvent) {
-          //replyMessage 使用次數+1
-          const replyMessageIncrement = await postBackEvent.ReplyMessage.increment(['replyMsgCount'], {
-            by: 1
-          })
-          //module 使用次數+1
-          const modulePostBackIncrement = await postBackEvent.ModulePostBack.increment(['moduleUsedCount'], {
-            by: 1
-          })
-
-          if (replyMessageIncrement && modulePostBackIncrement) {
-            return client.replyMessage(event.replyToken, postBackEvent.ReplyMessage.messageTemplate)
-          }
-        } else {
-          return client.replyMessage(event.replyToken, {
-            type: 'text',
-            text: '針對這個問題，我還沒辦法回答，你要不要先問我別的問題？'
-          })
-        }
-      } catch (err) {
-        console.log(err)
-        return client.replyMessage(replyToken, {
-          type: 'text',
-          text: '這個問題我還沒辦法回答，你要不要先問我別的問題？'
-        })
-      }
-    })
-    .catch(err => {
-      console.log(err)
+  switch (_data.action) {
+    case 'triggerReply':
+      return triggerReply(event, client, _data)
+    default:
       return client.replyMessage(event.replyToken, {
         type: 'text',
-        text: '系統異常，請稍後再試'
+        text: '針對這個問題，我還沒辦法回答，你要不要先問我別的問題？'
       })
-    })
+  }
 }
 
 module.exports = { handlePostback }
