d7c684a6ac9962be9812cd4a1bbf735101e0acf3
Gary Wu <s19003045@gmail.com>
2020-08-06 12:07:42 +0800

    歡迎訊息設定-API-取得歡迎訊息 & 建立 LINE 官方預設歡迎訊息schema

--- services/welcomeMsgService.js
+++ services/welcomeMsgService.js
@@ -4,15 +4,25 @@ const db = require('../models')
 const Chatbot = db.Chatbot
 const WelcomeMsg = db.WelcomeMsg
 const ReplyModule = db.ReplyModule
+// LINE default welcome message
+const welcomeMsgSchema = require('../templates/welcomeMsg/welcomeMsgSchema.json')
+
+// import helper
+const { Op } = require("sequelize");
+const { v4: uuidv4 } = require("uuid");
+
 
 const welcomeMsgService = {
   // 歡迎訊息設定-取得歡迎訊息資料
   getWelcomeMsg: async (req, res, callback) => {
     try {
       const { ChatbotId } = req.query
+
+      //  把 status 為 'edited'及'in-use'的資料都給前端，讓使用者自行決定是否啟用自訂歡迎訊息
       const welcomeMsg = await WelcomeMsg.findOne({
         where: {
-          ChatbotId: ChatbotId
+          ChatbotId: ChatbotId,
+          [Op.or]: [{ status: 'edited' }, { status: 'in-use' }],
         }
       })
 
@@ -26,9 +36,21 @@ const welcomeMsgService = {
         }
         callback(data)
       } else {
+        // 使用者若無自訂歡迎訊息，則建立一個
+        const welcomeMsgeCreate = await WelcomeMsg.create({
+          name: '歡迎訊息',
+          uuid: uuidv4(),
+          status: 'edited',
+          replyMessage: welcomeMsgSchema,
+          ChatbotId: ChatbotId
+        })
+
         const data = {
-          status: 'error',
-          message: '存取失敗'
+          status: 'success',
+          message: '使用者無自訂歡迎訊息。提供 LINE 官方預設訯迎訊息',
+          data: {
+            welcomeMsg: welcomeMsgeCreate
+          }
         }
         callback(data)
       }
