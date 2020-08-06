c0d42d7a5167d14e9c10ebe9a568960477488c84
Gary Wu <s19003045@gmail.com>
2020-08-06 12:05:42 +0800

    修改 Model：replyModule之moduleUsedCount定義 defaultValue

--- models/replymodule.js
+++ models/replymodule.js
@@ -3,7 +3,10 @@ module.exports = (sequelize, DataTypes) => {
   const ReplyModule = sequelize.define('ReplyModule', {
     name: DataTypes.STRING, // postback 名稱
     uuid: DataTypes.STRING,
-    moduleUsedCount: DataTypes.INTEGER, // 使用次數
+    moduleUsedCount: {
+      type: DataTypes.INTEGER,  //使用者呼叫次數
+      defaultValue: 0
+    },
     status: DataTypes.ENUM('edited', 'in-use', 'archived'),  //模組狀態
     replyMessage: {  // message json
       type: DataTypes.TEXT,
