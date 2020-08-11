364ded48b7a79ad794606efc0ef8b0fd0b0f9aad
Gary Wu <s19003045@gmail.com>
2020-08-12 00:22:00 +0800

    使用者驗證：建立 passport-local 及 passport-jwt 二種驗證機制

---
+++ config/passport.js
@@ -0,0 +1,60 @@
+const passport = require('passport')
+  , LocalStrategy = require('passport-local').Strategy;
+
+const db = require('../models')
+const ConsoleUser = db.ConsoleUser
+const bcrpyt = require('bcrypt')
+
+passport.use(new LocalStrategy({
+  usernameField: 'email',
+  passwordField: 'password',
+  passReqToCallback: true
+},
+  async function (req, username, password, done) {
+    try {
+      const user = await ConsoleUser.findOne({
+        where: {
+          email: username,
+        }
+      })
+
+      if (!user) {
+        return done(null, false, { message: '帳號或密碼錯誤' });
+      }
+      if (!bcrpyt.compareSync(password, user.password)) {
+        return done(null, false, { message: '帳號或密碼錯誤' })
+      }
+
+      return done(null, user);
+    } catch (err) {
+      if (err) { return done(err); }
+    }
+
+  }
+));
+
+const passportJWT = require('passport-jwt')
+const JwtStrategy = passportJWT.Strategy,
+  ExtractJwt = passportJWT.ExtractJwt;
+const jwtOptions = {}
+jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+jwtOptions.secretOrKey = process.env.JWT_SECRET;
+
+passport.use(new JwtStrategy(jwtOptions, async (jwt_payload, done) => {
+  try {
+    const user = await ConsoleUser.findByPk(jwt_payload.id)
+
+    if (user) {
+      return done(null, user);
+    } else {
+      return done(null, false);
+    }
+  } catch (err) {
+    if (err) {
+      return done(err, false);
+    }
+  }
+}
+));
+
+module.exports = passport
